72e42f8d08837fbeb098c513084b2ca7b07ee8bd
Stephen Cefali <stephencefali@gmail.com>
2016-09-23 11:27:28 -0700

    did some things to make things appear

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,28 @@
 // start slingin' some d3 here.
+
+
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+}
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+}
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+}
+
+var gameBoard = d3.select('.board').append('svg:svg')
+                .attr('width', gameOptions.width)
+                .attr('height', gameOptions.height);
+
+var updateScore = function() {
+  d3.select('#current-score').text(gameStats.score.toString())
+}
+
